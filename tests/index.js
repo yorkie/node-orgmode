7bdb1bd0574061fb46858e74e2b2581fa6766da3
Steven <sky.stevenwang@gmail.com>
2016-06-17 13:25:07 +0800

    more test file: tags2.org

--- tests/index.js
+++ tests/index.js
@@ -60,6 +60,18 @@ test('read a document with tags', function(t) {
   const h2 = doc.findByTitle('h2')[0];
   const h11 = doc.findByTitle('h11')[0];
   t.equal(h1.tags.length, 2);
+  t.equal(h2.tags.length, 0);
+  t.equal(h11.tags[0], "_3%");
+  t.end();
+});
+
+test('read a document with tags', function(t) {
+  const doc = new Orgmode(
+    path.join(__dirname, './documents/tags2.org'));
+  const h1 = doc.findByLevel(1)[0];
+  const h2 = doc.findByTitle('h2')[0];
+  const h11 = doc.findByTitle('h11')[0];
+  t.equal(h1.tags.length, 2);
   t.equal(h2.tags.length, 2);
   t.equal(h11.tags[0], "_3%");
   const tag2s = doc.findByTags('tag2');
