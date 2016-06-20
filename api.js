01cbd503ea01c17e6f5b4951b6455007bb1a2b95
yorkie <yorkiefixer@gmail.com>
2016-06-20 17:45:24 +0800

    docs

---
+++ api.js
@@ -0,0 +1,42 @@
+window.apimeta = {
+  "enums": [],
+  "classes": [
+    {
+      "name": "Orgmode",
+      "namespace": "Orgmode.Orgmode",
+      "module": "Orgmode",
+      "description": "<p>The main script of <code>Orgmode</code></p>\n"
+    },
+    {
+      "name": "OrgmodeParser",
+      "namespace": "Orgmode.OrgmodeParser",
+      "module": "Orgmode",
+      "description": ""
+    },
+    {
+      "name": "OutlineNode",
+      "namespace": "Orgmode.OutlineNode",
+      "module": "Orgmode",
+      "description": "<p>For every outline node</p>\n"
+    },
+    {
+      "name": "OutlinesArrayList",
+      "namespace": "Orgmode.OutlinesArrayList",
+      "module": "Orgmode",
+      "description": "<p>OutlineArrayList is a class to support some methods with\nsome <code>OutlineNode</code> instances.</p>\n"
+    },
+    {
+      "name": "SectionSplitter",
+      "namespace": "Orgmode.SectionSplitter",
+      "module": "Orgmode",
+      "description": ""
+    }
+  ],
+  "modules": [
+    {
+      "name": "Orgmode",
+      "namespace": "Orgmode",
+      "description": "<p>The <code>Orgmode</code> implementation for Node.js which contains:</p>\n<ol>\n<li>A parser for orgmode based on the monadic LL(infinity) parser combinator\nlibrary <a href=\"https://github.com/jneen/parsimmon\">jneen/parsimmon</a>.</li>\n<li>A simple search engine for outlines.</li>\n</ol>\n"
+    }
+  ]
+}
